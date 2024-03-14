2d275ad2f41497cc3c5200348835b4d3429eda75
RafaelB411 <rafaelb411.rb@gmail.com>
2024-03-14 14:55:59 -0300

    feat: add hardware mapping

---
+++ exemples/python/app-pci.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+import os, sys
+from fcntl import ioctl
+
+# ioctl commands defined at the pci driver
+RD_SWITCHES   = 24929
+RD_PBUTTONS   = 24930
+WR_L_DISPLAY  = 24931
+WR_R_DISPLAY  = 24932
+WR_RED_LEDS   = 24933
+WR_GREEN_LEDS = 24934
+
+def main():
+    if len(sys.argv) < 2:
+        print("Error: expected more command line arguments")
+        print("Syntax: %s </dev/device_file>"%sys.argv[0])
+        exit(1)
+
+    fd = os.open(sys.argv[1], os.O_RDWR)
+
+    # led vermelho
+    data = 0x00000000;
+    ioctl(fd, WR_RED_LEDS)
+    retval = os.write(fd, data.to_bytes(4, 'little'))
+    print("ledr: %d bytes"%retval)
+
+    # led verde
+    data = 0x00000000;
+    ioctl(fd, WR_GREEN_LEDS)
+    retval = os.write(fd, data.to_bytes(4, 'little'))
+    print("ledg: %d bytes"%retval)
+
+    # display grudado
+    data = 0x40404040;
+    ioctl(fd, WR_R_DISPLAY)
+    retval = os.write(fd, data.to_bytes(4, 'little'))
+    print("display2: %d bytes"%retval)
+
+    # display separado
+    data = 0x40404040;
+    ioctl(fd, WR_L_DISPLAY)
+    retval = os.write(fd, data.to_bytes(4, 'little'))
+    print("display1: %d bytes"%retval)
+
+    # botao
+    ioctl(fd, RD_PBUTTONS)
+    red = os.read(fd, 4); # read 4 bytes and store in red var
+    print("botoes: 0x%X"%int.from_bytes(red, 'little'))
+
+    # switch
+    ioctl(fd, RD_SWITCHES)
+    red = os.read(fd, 4);
+    print("switches: 0x%X"%int.from_bytes(red, 'little'))
+
+    os.close(fd)
+
+if __name__ == '__main__':
+    main()
+
